c14c4526f17984457b0d1f3cce7d3504b0c42bbb
Albin Theander <albin.theander@gmail.com>
2023-12-14 22:10:38 +0100

    Clean up day 12

--- typescript/src/days/day12.ts
+++ typescript/src/days/day12.ts
@@ -7,33 +7,37 @@ type Problem = {
 
 export function day12(data: string) {
     const problems = parseData(data);
-    part2(problems);
+    const value1 = part1(problems);
+    const value2 = part2(problems);
+
+    console.log('The total numbers of solutions is', value1);
+    console.log('The solutions to the 5x-problems is', value2);
 }
 
 function part1(problems: Problem[]) {
     const result = problems.map(p => solve(p.line, p.hints)).reduce(sum);
-    console.log(result);
+    return result;
 }
 
 let cache = new Map<string, number>();
+
 function part2(problems: Problem[]) {
     const longerProblems = problems.map(({ line, hints }) => {
         const longLine = [line, line, line, line, line].join('?');
         const longHints = [hints, hints, hints, hints, hints].flat();
         return { line: longLine, hints: longHints }
     });
+
     const result = longerProblems.map((p, i) => {
-        console.log('Solving', i, p);
         cache = new Map<string, number>();
         return solve(p.line, p.hints)
     }).reduce(sum);
-    console.log(result);
+    return result;
 }
 
 function solve(line: string, hints: number[], first = true): number {
     let key = JSON.stringify([line, hints]);
     if (cache.has(key)) return cache.get(key);
-    // console.log(line, hints);
     if (hints.length === 0) {
         const hasHashes = line.includes('#');
         return hasHashes ? 0 : 1;
@@ -52,7 +56,6 @@ function solve(line: string, hints: number[], first = true): number {
     const restHints = hints.slice(1);
     let foundHash = false;
     let count = 0;
-    // console.log('Looking for', line.slice(p), p, hint);
     while(p + hint <= line.length && !foundHash) {
         if (foundHash && line[p] === '.') break;
         foundHash = foundHash || line[p] === '#';
@@ -61,17 +64,14 @@ function solve(line: string, hints: number[], first = true): number {
         }
         p++;
     }
-    // console.log('Done', line, hints, count);
     cache.set(key, count);
     return count;
 }
 
 function validHint(s: string, pos: number, hint: number): boolean {
-    // console.log('Validating', s.slice(pos), hint);
     for (let i = pos; i < pos + hint; i++)
         if (i >= s.length || s[i] === '.') return false;
 
-    // console.log(s.slice(pos), hint, 'is valid');
     return true;
 }
 
