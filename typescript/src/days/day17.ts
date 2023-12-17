89bb76c6a51af34f7ee4f2b7d6e7472e26bad23b
Albin Theander <albin.theander@gmail.com>
2023-12-17 13:09:39 +0100

    Day 17 - cleanup typescript

--- typescript/src/days/day17.ts
+++ typescript/src/days/day17.ts
@@ -11,85 +11,73 @@ export function day17(data: string) {
     console.log('The minimum heat loss for the ultra crucible is', result2);
 }
 
-type State = {
-    x: number,
-    y: number,
-    dx: number,
-    dy: number,
-    steps: number,
-    heat: number
-};
-
 function part1(map: Array2D<number, number>) {
-    const best = new DefaultMap<State, number>(
-        (a) => [a.x, a.y, a.dx, a.dy, a.steps].toString(),
-        Infinity
-    );
-
-    const toTry = new PriorityQueue<State>((s1, s2) => s1.heat < s2.heat);
-    toTry.add({x: 0, y: 0, dx: 1, dy: 0, steps: 0, heat: 0})
-    toTry.add({x: 0, y: 0, dx: 0, dy: 1, steps: 0, heat: 0})
-    
-    while(toTry.size > 0) {
-        const state = toTry.pop();
-        const {x, y, dx, dy, steps, heat} = state;
-        if (best.get(state) <= heat) continue;
-        best.set(state, heat);
-
-        if (x == map.width-1 && y == map.height-1) {
-            return heat;
-        }
-        if (steps < 3 && map.contains(x+dx, y+dy)) {
-            const newHeat = heat + map.get(x+dx, y+dy);
-            toTry.add({x: x+dx, y: y+dy, dx, dy, steps: steps+1, heat: newHeat });
-        }
-        const [leftDx, leftDy] = [dy, -dx];
-        if (map.contains(x + leftDx, y + leftDy)) {
-            const newHeat = heat + map.get(x+leftDx, y+leftDy);
-            toTry.add({x: x+leftDx, y: y+leftDy, dx: leftDx, dy: leftDy, steps: 1, heat: newHeat})
-        }
-        const [rightDx, rightDy] = [-dy, dx];
-        if (map.contains(x + rightDx, y + rightDy)) {
-            const newHeat = heat + map.get(x+rightDx, y+rightDy);
-            toTry.add({x: x+rightDx, y: y+rightDy, dx: rightDx, dy: rightDy, steps: 1, heat: newHeat})
-        }
-    }
-    console.log('Awwwww');
+    return getMinimumHeatLoss(map, 0, 3);
 }
 
 function part2(map: Array2D<number, number>) {
+    return getMinimumHeatLoss(map, 4, 10);
+}
+
+type State = {
+    x: number, // Position
+    y: number,
+    dx: number, // Direction
+    dy: number,
+    steps: number, // Taken steps in the current direction
+    heatLoss: number // Total heatloss so far
+};
+
+function getMinimumHeatLoss(map: Array2D<number, number>, minStepsForward: number, maxStepsForward: number) {
+    // Keep track of the best heat values for each state
     const best = new DefaultMap<State, number>(
         (a) => [a.x, a.y, a.dx, a.dy, a.steps].toString(),
         Infinity
     );
 
-    const toTry = new PriorityQueue<State>((s1, s2) => s1.heat < s2.heat);
-    toTry.add({x: 0, y: 0, dx: 1, dy: 0, steps: 0, heat: 0})
-    toTry.add({x: 0, y: 0, dx: 0, dy: 1, steps: 0, heat: 0})
+    // A priority queue with states that we should try, sorted by heatloss.
+    // Since we are trying the minimum things first, this means that we will
+    // have found the optimal solution as soon as we reach the goal.
+    const toTry = new PriorityQueue<State>((s1, s2) => s1.heatLoss < s2.heatLoss);
+    toTry.add({x: 0, y: 0, dx: 1, dy: 0, steps: 0, heatLoss: 0})
+    toTry.add({x: 0, y: 0, dx: 0, dy: 1, steps: 0, heatLoss: 0})
     
+    // We'll never run out of states to try, unless we did something wrong. :-)
     while(toTry.size > 0) {
         const state = toTry.pop();
-        const {x, y, dx, dy, steps, heat} = state;
-        if (best.get(state) <= heat) continue;
-        best.set(state, heat);
+        const {x, y, dx, dy, steps, heatLoss} = state;
 
+        // If we already visited this state with a lower heatloss, no need to try again
+        // Note: This cutoff can probably be better optimized, by checking all states
+        // in this position and direction with a lower number of steps.
+        if (best.get(state) <= heatLoss) continue;
+        best.set(state, heatLoss);
+
+        // Did we reach our goal? Woohoo!!!
         if (x == map.width-1 && y == map.height-1) {
-            return heat;
+            return heatLoss;
         }
-        if (steps < 10 && map.contains(x+dx, y+dy)) {
-            const newHeat = heat + map.get(x+dx, y+dy);
-            toTry.add({x: x+dx, y: y+dy, dx, dy, steps: steps+1, heat: newHeat });
+
+        // Forward
+        if (steps < maxStepsForward && map.contains(x+dx, y+dy)) {
+            const newHeat = heatLoss + map.get(x+dx, y+dy);
+            toTry.add({x: x+dx, y: y+dy, dx, dy, steps: steps+1, heatLoss: newHeat });
         }
+
+        // Turn left
         const [leftDx, leftDy] = [dy, -dx];
-        if (steps >= 4 && map.contains(x + leftDx, y + leftDy)) {
-            const newHeat = heat + map.get(x+leftDx, y+leftDy);
-            toTry.add({x: x+leftDx, y: y+leftDy, dx: leftDx, dy: leftDy, steps: 1, heat: newHeat})
+        if (steps >= minStepsForward && map.contains(x + leftDx, y + leftDy)) {
+            const newHeat = heatLoss + map.get(x+leftDx, y+leftDy);
+            toTry.add({x: x+leftDx, y: y+leftDy, dx: leftDx, dy: leftDy, steps: 1, heatLoss: newHeat})
         }
+
+        // Turn right
         const [rightDx, rightDy] = [-dy, dx];
-        if (steps >= 4 && map.contains(x + rightDx, y + rightDy)) {
-            const newHeat = heat + map.get(x+rightDx, y+rightDy);
-            toTry.add({x: x+rightDx, y: y+rightDy, dx: rightDx, dy: rightDy, steps: 1, heat: newHeat})
+        if (steps >= minStepsForward && map.contains(x + rightDx, y + rightDy)) {
+            const newHeat = heatLoss + map.get(x+rightDx, y+rightDy);
+            toTry.add({x: x+rightDx, y: y+rightDy, dx: rightDx, dy: rightDy, steps: 1, heatLoss: newHeat})
         }
     }
+    // We should never get here!
     console.log('Awwwww');
 }
